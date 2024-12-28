f3239e1c5642c425ec167fd6e6097568875fff0f
Sarah Evermore <sarah.evermore.02@gmail.com>
2024-12-28 17:42:52 +0100

    update lazyvim, get k8s-snippets to work :3

--- lua/plugins/yaml_kube.lua
+++ lua/plugins/yaml_kube.lua
@@ -1,28 +1,19 @@
-return {
-  --   {
-  --     "someone-stole-my-name/yaml-companion.nvim",
-  --     init = function(plugin)
-  --       require("telescope").load_extension("yaml_schema")
-  --     end,
-  --     opts = {
-  --       schemas = {
-  --         kubernetes = "*.yaml",
-  --         ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
-  --         ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
-  --         ["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/*.{yml,yaml}",
-  --         ["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
-  --         ["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
-  --         ["http://json.schemastore.org/ansible-playbook"] = "*play*.{yml,yaml}",
-  --         ["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
-  --         ["https://json.schemastore.org/dependabot-v2"] = ".github/dependabot.{yml,yaml}",
-  --         ["https://json.schemastore.org/gitlab-ci"] = "*gitlab-ci*.{yml,yaml}",
-  --         ["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] = "*api*.{yml,yaml}",
-  --         ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "*docker-compose*.{yml,yaml}",
-  --         ["https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json"] = "*flow*.{yml,yaml}",
-  --       },
-  --     },
-  --   },
+local function tableMerge(t1, t2)
+  for k, v in pairs(t2) do
+    if type(v) == "table" then
+      if type(t1[k] or false) == "table" then
+        tableMerge(t1[k] or {}, t2[k] or {})
+      else
+        t1[k] = v
+      end
+    else
+      t1[k] = v
+    end
+  end
+  return t1
+end
 
+return {
   {
     "neovim/nvim-lspconfig",
     ---@class PluginLspOpts
@@ -50,12 +41,31 @@ return {
       },
     },
   },
-  { "budimanjojo/k8s-snippets" },
+
   {
-    "garymjr/nvim-snippets",
-    opts = {
-      friendly_snippets = true,
-    },
+    "saghen/blink.cmp",
     dependencies = { "rafamadriz/friendly-snippets", "budimanjojo/k8s-snippets" },
+    opts = function(_, opts)
+      -- https://cmp.saghen.dev/configuration/snippets.html#custom-snippets
+      local new_opts = {
+        sources = {
+          providers = {
+            snippets = {
+              opts = {
+                search_paths = {},
+              },
+            },
+          },
+        },
+      }
+
+      for _, el in ipairs(vim.api.nvim_list_runtime_paths()) do
+        if string.match(el, "k8s.snippets") then
+          table.insert(new_opts.sources.providers.snippets.opts.search_paths, el)
+        end
+      end
+      -- TODO: use `vim.tbl_deep_extend()`
+      tableMerge(opts, new_opts)
+    end,
   },
 }
