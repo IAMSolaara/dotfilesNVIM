0c52204158397eb4172b0800c6698002cfaaca67
Sarah Evermore <sarah.evermore.02@gmail.com>
2025-04-16 12:58:01 +0200

    :3

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -57,7 +57,6 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = { "catppuccin-macchiato" } },
   checker = { enabled = true }, -- automatically check for plugin updates
   performance = {
     rtp = {
