50816624cb7e96580855623624e4ec023f37f7c2
Sarah Evermore <sarah.evermore.02@gmail.com>
2025-04-16 12:58:05 +0200

    neovide options :3

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,3 +7,11 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 0
 vim.opt.expandtab = false
 vim.opt.shiftwidth = 4
+
+if vim.g.neovide then
+  vim.o.guifont = "DejaVuSansM Nerd Font"
+  vim.g.neovide_opacity = 0.83
+  vim.g.neovide_normal_opacity = 0.83
+  vim.g.neovide_window_blurred = true
+  vim.g.neovide_input_macos_option_key_is_meta = "only_left"
+end
