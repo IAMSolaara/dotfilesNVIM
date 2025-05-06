897491a068daba58bfbd9cba9c1f9587f70c7c18
Solaara Evermore <evermore@solaara.net>
2025-05-06 15:04:51 +0200

    move options to nix :3

---
+++ config/options.nix
@@ -0,0 +1,16 @@
+{
+  opts = {
+    relativenumber = false;
+    tabstop = 4;
+    softtabstop = 0;
+    expandtab = false;
+    shiftwidth = 4;
+    guifont = "DejaVuSansM Nerd Font";
+  };
+  globals = {
+    neovide_opacity = 0.83;
+    neovide_normal_opacity = 0.83;
+    neovide_window_blurred = true;
+    neovide_input_macos_option_key_is_meta = "only_left";
+  };
+}
