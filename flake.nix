e82efc17013b2956b0855f7a34ebdad80ccb1581
Solaara Evermore <evermore@solaara.net>
2025-05-08 12:26:22 +0200

    add caches :3

--- flake.nix
+++ flake.nix
@@ -1,6 +1,17 @@
 {
   description = "A nixvim configuration";
 
+  nixConfig = {
+	  extra-substituters = [
+		  "https://nix-community.cachix.org"
+		  "https://solaaradotnet.cachix.org"
+	  ];
+	  extra-trusted-public-keys = [
+		  "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+		  "solaaradotnet.cachix.org-1:ySwixOoVu7Fy9DggLJDza0u6JsvfQ9Gn4WVcTYOLxes="
+	  ];
+  };
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     nixvim.url = "github:nix-community/nixvim";
